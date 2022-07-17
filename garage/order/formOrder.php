625e0d9b3b6e7d78fcd6943947d3c8d6a68258d6
Vojtěch divoký <vojtech.divoky@polyweb.cz>
2022-07-17 16:49:44 +0200

    order list done mby, del order done, form order wip

---
+++ garage/order/formOrder.php
@@ -0,0 +1,109 @@
+<?php
+require_once "../../config.php";
+session_start();
+if(!isset($_SESSION["currentUser"]) || !$_SESSION["currentUser"]["employee"]){
+    header("Location: ../user/login.php");
+}
+?>
+<!DOCTYPE html>
+<html lang="cz">
+
+<head>
+    <meta charset="UTF-8">
+    <title><?php echo $_GET["add"] ? "Přidat" : "Upravit"; ?> objednávku</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link href="../../styles/custom.min.css" rel="stylesheet">
+    <link href="../../styles/index.css" rel="stylesheet">
+</head>
+
+(admin only) id_customer
+(admin only) id_employee
+id_batch
+thirds
+pints
+
+id_beer
+label
+thirds
+pints
+id_status
+
+label
+
+beer => label
+
+<body class="m-5 p-5 text-light">
+    <h1><?php echo $_GET["add"] ? "Přidat" : "Upravit"; ?> objednávku</h1>
+    <a class="btn btn-outline-primary" href="orderList.php"><i class="bi bi-arrow-left-circle pe-2"></i>Zpět</a> <!-- TODO ošetřit že jsem admin, jinak vracet na homepage -->
+    <form class="needs-validation mt-3" novalidate action=<?php echo $_GET["add"] ? "addOrderScript.php" : "editOrderScript.php?orderId=" . $_GET["orderId"]; ?> method="post">
+        <div class="mb-3 form-floating">
+            <select class="form-select" id="beer" name="beer">
+                <?php
+                $sql = "SELECT id, label FROM beer;";
+                if ($result = mysqli_query($link, $sql)) {
+                    while ($row = mysqli_fetch_row($result)) {
+                        echo "<option value='" . $row[0] . "'" . (!$_GET["add"] ? ($_SESSION["batches"][$_GET["batchId"]]["beerId"] == $row[0] ? " selected" : "") : "") .">" . $row[1] . "</option>";
+                    }
+                    mysqli_free_result($result);
+                }
+                ?>
+            </select>
+            <label for="beer" class="form-label">Pivo</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="text" class="form-control" id="label" name="label" required maxlength="50" value="<?php echo !$_GET["add"] ? $_SESSION["batches"][$_GET["batchId"]]["batchLabel"] : ""; ?>">
+            <label for="label" class="form-label">Název</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="date" class="form-control" id="created" name="created" required value="<?php echo !$_GET["add"] ? $_SESSION["batches"][$_GET["batchId"]]["created"] : ""; ?>">
+            <label for="created" class="form-label">Vařeno</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="number" class="form-control" id="thirds" name="thirds" value="<?php echo !$_GET["add"] ? $_SESSION["batches"][$_GET["batchId"]]["thirds"] : ""; ?>">
+            <label for="label" class="form-label">Třetinky [ks]</label>
+        </div>
+        <div class="form-floating mb-3">
+            <input type="number" class="form-control" id="pints" name="pints" value="<?php echo !$_GET["add"] ? $_SESSION["batches"][$_GET["batchId"]]["pints"] : ""; ?>">
+            <label for="pints" class="form-label">Půllitry [ks]</label>
+        </div>
+        <div class="mb-3 form-floating">
+            <select class="form-select" id="status" name="status">
+                <?php
+                $sql = "SELECT id, label FROM status_batch;";
+                if ($result = mysqli_query($link, $sql)) {
+                    while ($row = mysqli_fetch_row($result)) {
+                        echo "<option value='" . $row[0] . "'" . (!$_GET["add"] ? ($_SESSION["batches"][$_GET["batchId"]]["statusId"] == $row[0] ? " selected" : "") : "") .">" . $row[1] . "</option>";
+                    }
+                    mysqli_free_result($result);
+                }
+                mysqli_close($link);
+                ?>
+            </select>
+            <label for="status" class="form-label">Status</label>
+        </div>
+        <button type="submit" class="btn btn-success"><i class="pe-2 bi bi-<?php echo $_GET["add"] ? "plus-circle" : "pencil"; ?>"></i><?php echo $_GET["add"] ? "Přidat" : "Upravit"; ?> objednávku</button>
+    </form>
+</body>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ho+j7jyWK8fNQe+A12Hb8AhRq26LrZ/JpcUGGOn+Y7RsweNrtN/tE3MoK7ZeZDyx" crossorigin="anonymous"></script>
+<script>
+    (function() {
+        "use strict"
+        var forms = document.querySelectorAll(".needs-validation")
+        Array.prototype.slice.call(forms)
+            .forEach(function(form) {
+                form.addEventListener("submit", function(event) {
+                    if (!form.checkValidity()) {
+                        event.preventDefault()
+                        event.stopPropagation()
+                    }
+
+                    form.classList.add("was-validated")
+                }, false)
+            })
+    })()
+</script>
+
+</html>
---
+++ garage/order/orderList.php
@@ -0,0 +1,134 @@
+<?php
+require_once "../../config.php";
+session_start();
+if (!isset($_SESSION["currentUser"]) || !$_SESSION["currentUser"]["employee"]) {
+    header("Location: ../user/login.php");
+}
+
+$sql = "SELECT bo.id, bo.thirds, bo.pints, c.mail, c.f_name, c.l_name, c.instagram, e.id, e.f_name, e.l_name, b.label, b.thirds, b.pints, bs.label, bs.color, os.label, os.color
+        FROM beer_order bo INNER JOIN user c ON bo.id_customer=c.id INNER JOIN user e ON bo.id_employee=e.id INNER JOIN batch b ON bo.id_batch=b.id
+        INNER JOIN status_batch bs ON b.id_status=bs.id INNER JOIN status_order os ON bo.id_status=os.id;";
+$orders = [];
+if ($result = mysqli_query($link, $sql)) {
+    while ($row = mysqli_fetch_row($result)) {
+        $orders[$row[0]] = [
+            "thirds" => $row[1],
+            "pints" => $row[2],
+            "customer" => [
+                "mail" => $row[3],
+                "name" => ($row[4] . " " . $row[5]),
+                "instagram" => $row[6]
+            ],
+            "employee" => [
+                "id" => $row[7],
+                "name" => ($row[8] . " " . $row[9]),
+            ],
+            "batch" => [
+                "label" => $row[10],
+                "thirds" => $row[11],
+                "pints" => $row[12],
+                "status" => [
+                    "label" => $row[13],
+                    "color" => $row[14]
+                ]
+            ],
+            "status" => [
+                "label" => $row[15],
+                "color" => $row[16]
+            ]
+        ];
+    }
+    mysqli_free_result($result);
+}
+mysqli_close($link);
+?>
+<!DOCTYPE html>
+<html lang="cz">
+
+<head>
+    <meta charset="UTF-8">
+    <title>Objednávky</title>
+    <meta name="viewport" content="width=device-width,initial-scale=1">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.7.2/font/bootstrap-icons.css">
+    <link href="../../styles/custom.min.css" rel="stylesheet">
+    <link href="../../styles/index.css" rel="stylesheet">
+</head>
+
+<body class="m-5 p-5 text-light">
+    <h1>Objednávky</h1>
+    <a class="btn btn-outline-primary" href="../homepage.php"><i class="bi bi-arrow-left-circle pe-2"></i>Zpět</a>
+    <a class="btn btn-outline-success" href="formOrder.php?add=1"><i class="bi bi-plus-circle pe-2"></i>Přidat</a>
+    <div class="table-responsive">
+        <table class="mt-3 table table-striped table-hover table-dark">
+            <caption>Seznam objednávek</caption>
+            <thead class="table-dark">
+                <tr>
+                    <th scope="col">#</th>
+                    <th scope="col">Zákazník</th>
+                    <th scope="col">Várka</th>
+                    <th scope="col">Status várky</th>
+                    <th scope="col">Třetinky [ks/várka ks]</th>
+                    <th scope="col">Půllitry [ks/várka ks]</th>
+                    <th scope="col">Řeší</th>
+                    <th scope="col">Status</th>
+                    <th scope="col"></th>
+                    <th scope="col"></th>
+                </tr>
+            </thead>
+            <tbody>
+                <?php
+                $_SESSION["orders"] = $orders;
+                foreach ($orders as $key => $order) {
+                    echo '<tr>
+                            <th scope="row">' . $key . '</th>
+                            <td>' . $order["customer"]["name"] . '<a class="btn btn-info"><i class="bi bi-search"></i></a></td>
+                            <td>' . $order["batch"]["label"] . '</td>
+                            <td><span class="ms-2 badge rounded-pill" style="background-color:#' . $order["batch"]["status"]["color"] . ';">' . $order["batch"]["status"]["label"] . '</td>
+                            <td>' . $order["thirds"] . "/" . $order["batch"]["thirds"] . '</td>
+                            <td>' . $order["pints"] . "/" . $order["batch"]["pints"] . '</td>
+                            <td>' . $order["employee"]["name"] . '</td>
+                            <td><span class="ms-2 badge rounded-pill" style="background-color:#' . $order["status"]["color"] . ';">' . $order["status"]["label"] . '</td>
+                            <td><a class="btn btn-outline-secondary" href="formOrder.php?orderId=' . $key . '"><i class="bi bi-pencil"></i></a></td>
+                            <td><a class="btn btn-outline-danger deleteBtn" data-order-id=' . $key . '><i class="bi bi-trash"></i></a></td>
+                        </tr>';
+                }
+                ?>
+            </tbody>
+        </table>
+    </div>
+
+    <div class="modal fade" id="confDeleteModal" tabindex="-1" aria-labelledby="confDeleteModalLabel" aria-hidden="true">
+        <div class="modal-dialog modal-sm modal-dialog-centered">
+            <div class="modal-content">
+                <div class="modal-header">
+                    <h5 class="modal-title" id="exampleModalLabel">Opravdu?</h5>
+                </div>
+                <div class="modal-body">
+                    Skutečně chcete odstranit várku ze systému?
+                </div>
+                <div class="modal-footer">
+                    <button type="button" class="btn btn-outline-secondary" data-bs-dismiss="modal">Zavřít</button>
+                    <button type="button" class="btn btn-outline-danger" id="confDeleteBtn">Odstranit</button>
+                </div>
+            </div>
+        </div>
+    </div>
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+    <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.6.0/jquery.min.js"></script>
+    <script>
+        $(document).ready(function() {
+            var orderId;
+            $(".deleteBtn").click(function() {
+                orderId = $(this).data("orderId");
+                $('#confDeleteModal').modal('show');
+            });
+
+            $("#confDeleteBtn").click(function() {
+                window.location = "delOrderScript?id=" + orderId;
+            });
+        });
+    </script>
+</body>
+
+</html>
